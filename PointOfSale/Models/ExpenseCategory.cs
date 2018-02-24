c8efceef7d879645b75a3b2ccf2909fd4efe6e85
alimzaman <35924635+alimzaman@users.noreply.github.com>
2018-02-24 13:45:10 +0600

    UpdateExpenseCategory

--- PointOfSale/Models/ExpenseCategory.cs
+++ PointOfSale/Models/ExpenseCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,16 @@ namespace PointOfSale.Models
     public class ExpenseCategory
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string CategoryName { get; set; }
+        [Required]
+        [Range(1000,9999999)]
         public int CategoryCode { get; set; }
-        public string categoryDescription { get; set; }
+        public string CategoryDescription { get; set; }
         public string CategoryType { get; set; }
         public int? Parentid { get; set; }
-       
+        public string ParentName { get; set; }
+        public bool IsDelete { get; set; }        
     }
 }
---
+++ PointOfSale/UI/LogInUI.cs
@@ -0,0 +1,48 @@
+﻿using PointOfSale.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PointOfSale.UI
+{
+    public partial class LogInUI : Form
+    {
+        public LogInUI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text == null)
+            {
+                MessageBox.Show("Please Fill Up User Name");
+            }
+            if (txtPassword.Text == null)
+            {
+                MessageBox.Show("Please Fill Up Password");
+            }
+            else
+            {
+                string userName = txtUserName.Text;
+                string password = txtPassword.Text;
+                if (userName == "BKIICT" && password == "admin")
+                {
+                    SuperShopUI posManagementSystem = new SuperShopUI();
+                    posManagementSystem.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid UserName or Password");
+                }
+
+            }
+        }
+    }
+}
